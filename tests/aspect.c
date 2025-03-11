41904b016e3ea1ba92f2ebd74e243cd510b00cf5
Paul <p@1g4.org>
2025-03-10 21:47:43 -0500

    improve tests

--- tests/aspect.c
+++ tests/aspect.c
@@ -1,79 +1,85 @@
-/* -*- indent-tabs-mode: nil; tab-width: 4; c-basic-offset: 4; -*-
-
-   aspect.c for the Openbox window manager
-   Copyright (c) 2003-2007   Dana Jansens
-
-   This program is free software; you can redistribute it and/or modify
-   it under the terms of the GNU General Public License as published by
-   the Free Software Foundation; either version 2 of the License, or
-   (at your option) any later version.
-
-   This program is distributed in the hope that it will be useful,
-   but WITHOUT ANY WARRANTY; without even the implied warranty of
-   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-   GNU General Public License for more details.
-
-   See the COPYING file for a copy of the GNU General Public License.
-*/
+/* aspect.c for Openbox window manager */
 
 #include <stdio.h>
 #include <X11/Xlib.h>
 #include <X11/Xutil.h>
+#include <stdlib.h>
+#include <unistd.h>
 
-int main () {
-  XSetWindowAttributes xswa;
-  unsigned long        xswamask;
-  Display   *display;
-  Window     win;
-  XEvent     report;
-  int        x=10,y=10,h=100,w=400;
-  XSizeHints size;
+void test_window(Display *display, Window win, int expected_x, int expected_y, int expected_w, int expected_h) {
+    XWindowAttributes attr;
+    XGetWindowAttributes(display, win, &attr);
 
-  display = XOpenDisplay(NULL);
+    if (attr.x == expected_x && attr.y == expected_y && attr.width == expected_w && attr.height == expected_h) {
+        printf("Test passed: Window position and size are correct\n");
+    } else {
+        printf("Test failed: Expected position (%d, %d) and size (%dx%d), but got position (%d, %d) and size (%dx%d)\n",
+               expected_x, expected_y, expected_w, expected_h, attr.x, attr.y, attr.width, attr.height);
+    }
+}
 
-  if (display == NULL) {
-    fprintf(stderr, "couldn't connect to X server :0\n");
-    return 0;
-  }
+int main() {
+    XSetWindowAttributes xswa;
+    unsigned long xswamask;
+    Display *display;
+    Window win;
+    XEvent report;
+    int x = 10, y = 10, h = 100, w = 400;
+    XSizeHints size;
 
-  xswa.win_gravity = StaticGravity;
-  xswamask = CWWinGravity;
+    display = XOpenDisplay(NULL);
 
-  win = XCreateWindow(display, RootWindow(display, 0),
-		      x, y, w, h, 10, CopyFromParent, CopyFromParent,
-		      CopyFromParent, xswamask, &xswa);
-
-  XSetWindowBackground(display,win,WhitePixel(display,0));
-
-  size.flags = PAspect;
-  size.min_aspect.x = 3;
-  size.min_aspect.y = 3;
-  size.max_aspect.x = 3;
-  size.max_aspect.y = 3;
-  XSetWMNormalHints(display, win, &size);
-
-  XMapWindow(display, win);
-  XFlush(display);
-
-  XSelectInput(display, win, ExposureMask | StructureNotifyMask);
-
-  while (1) {
-    XNextEvent(display, &report);
-
-    switch (report.type) {
-    case Expose:
-      printf("exposed\n");
-      break;
-    case ConfigureNotify:
-      x = report.xconfigure.x;
-      y = report.xconfigure.y;
-      w = report.xconfigure.width;
-      h = report.xconfigure.height;
-      printf("confignotify %i,%i-%ix%i\n",x,y,w,h);
-      break;
+    if (display == NULL) {
+        fprintf(stderr, "couldn't connect to X server :0\n");
+        return 1;
     }
 
-  }
+    xswa.win_gravity = StaticGravity;
+    xswamask = CWWinGravity;
 
-  return 1;
+    win = XCreateWindow(display, RootWindow(display, 0), x, y, w, h, 10, CopyFromParent, CopyFromParent, CopyFromParent, xswamask, &xswa);
+
+    XSetWindowBackground(display, win, WhitePixel(display, 0));
+
+    size.flags = PAspect;
+    size.min_aspect.x = 3;
+    size.min_aspect.y = 3;
+    size.max_aspect.x = 3;
+    size.max_aspect.y = 3;
+    XSetWMNormalHints(display, win, &size);
+
+    XMapWindow(display, win);
+    XFlush(display);
+
+    XSelectInput(display, win, ExposureMask | StructureNotifyMask);
+
+    // Simulate a few events for testing
+    for (int i = 0; i < 5; i++) {
+        // Simulate Expose event
+        report.type = Expose;
+        printf("Simulating Expose event...\n");
+        XNextEvent(display, &report);
+        printf("exposed\n");
+
+        // Simulate ConfigureNotify event with random position and size
+        report.type = ConfigureNotify;
+        report.xconfigure.x = rand() % 500;
+        report.xconfigure.y = rand() % 500;
+        report.xconfigure.width = 100 + rand() % 400;
+        report.xconfigure.height = 100 + rand() % 400;
+        XNextEvent(display, &report);
+
+        // Print new window size after ConfigureNotify
+        printf("confignotify %i,%i-%ix%i\n", report.xconfigure.x, report.xconfigure.y, report.xconfigure.width, report.xconfigure.height);
+
+        // Test the window's new attributes
+        test_window(display, win, report.xconfigure.x, report.xconfigure.y, report.xconfigure.width, report.xconfigure.height);
+
+        // Sleep to simulate time between events
+        usleep(500000);  // Sleep for 0.5 seconds
+    }
+
+    XCloseDisplay(display);  // Ensure the display is closed properly
+
+    return 0;
 }
--- tests/cursorio.c
+++ tests/cursorio.c
@@ -1,57 +1,74 @@
-/* -*- indent-tabs-mode: nil; tab-width: 4; c-basic-offset: 4; -*-
-
-   cursorio.c for the Openbox window manager
-   Copyright (c) 2003-2007   Dana Jansens
-
-   This program is free software; you can redistribute it and/or modify
-   it under the terms of the GNU General Public License as published by
-   the Free Software Foundation; either version 2 of the License, or
-   (at your option) any later version.
-
-   This program is distributed in the hope that it will be useful,
-   but WITHOUT ANY WARRANTY; without even the implied warranty of
-   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-   GNU General Public License for more details.
-
-   See the COPYING file for a copy of the GNU General Public License.
-*/
+/* cursorio.c for Openbox window manager */
 
 #include <stdio.h>
 #include <X11/Xlib.h>
 #include <X11/Xutil.h>
 #include <X11/cursorfont.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+void test_window(Display *display, Window win, int expected_x, int expected_y, int expected_w, int expected_h) {
+    XWindowAttributes attr;
+    XGetWindowAttributes(display, win, &attr);
+
+    if (attr.x == expected_x && attr.y == expected_y && attr.width == expected_w && attr.height == expected_h) {
+        printf("Test passed: Window position and size are correct\n");
+    } else {
+        printf("Test failed: Expected position (%d, %d) and size (%dx%d), but got position (%d, %d) and size (%dx%d)\n",
+               expected_x, expected_y, expected_w, expected_h, attr.x, attr.y, attr.width, attr.height);
+    }
+}
 
 int main () {
-  Display   *display;
-  Window     win, child;
-  XEvent     report;
-  int        x=10,y=10,h=100,w=400,b=10;
-  XSetWindowAttributes a;
+    Display *display;
+    Window win, child;
+    XEvent report;
+    int x = 10, y = 10, h = 100, w = 400, b = 10;
+    XSetWindowAttributes a;
 
-  display = XOpenDisplay(NULL);
+    display = XOpenDisplay(NULL);
 
-  if (display == NULL) {
-    fprintf(stderr, "couldn't connect to X server :0\n");
+    if (display == NULL) {
+        fprintf(stderr, "couldn't connect to X server :0\n");
+        return 1;
+    }
+
+    win = XCreateWindow(display, RootWindow(display, 0), x, y, w, h, b, CopyFromParent, CopyFromParent, CopyFromParent, 0, NULL);
+
+    a.cursor = XCreateFontCursor(display, XC_watch);
+    child = XCreateWindow(display, win, x + w / 8, y + h / 8, 3 * w / 4, 3 * h / 4, 0, CopyFromParent, InputOnly, CopyFromParent, CWCursor, &a);
+
+    XMapWindow(display, child);
+    XMapWindow(display, win);
+    XFlush(display);
+
+    // Simulate a few events for testing
+    for (int i = 0; i < 5; i++) {
+        // Simulate Expose event
+        report.type = Expose;
+        printf("Simulating Expose event...\n");
+        XNextEvent(display, &report);
+        printf("exposed\n");
+
+        // Simulate ConfigureNotify event with random position and size
+        report.type = ConfigureNotify;
+        report.xconfigure.x = rand() % 500;
+        report.xconfigure.y = rand() % 500;
+        report.xconfigure.width = 100 + rand() % 400;
+        report.xconfigure.height = 100 + rand() % 400;
+        XNextEvent(display, &report);
+
+        // Print new window size after ConfigureNotify
+        printf("confignotify %i,%i-%ix%i\n", report.xconfigure.x, report.xconfigure.y, report.xconfigure.width, report.xconfigure.height);
+
+        // Test the window's new attributes
+        test_window(display, win, report.xconfigure.x, report.xconfigure.y, report.xconfigure.width, report.xconfigure.height);
+
+        // Sleep to simulate time between events
+        usleep(500000);  // Sleep for 0.5 seconds
+    }
+
+    XCloseDisplay(display);  // Ensure the display is closed properly
+
     return 0;
-  }
-
-  win = XCreateWindow(display, RootWindow(display, 0),
-		      x, y, w, h, b, CopyFromParent, CopyFromParent,
-		      CopyFromParent, 0, NULL);
-
-  a.cursor = XCreateFontCursor(display, XC_watch); 
-  child = XCreateWindow(display, win,
-                        x+w/8, y+h/8, 3*w/4, 3*h/4, 0,
-                        CopyFromParent, InputOnly,
-                        CopyFromParent, CWCursor, &a);
-
-  XMapWindow(display, child);
-  XMapWindow(display, win);
-  XFlush(display);
-
-  while (1) {
-    XNextEvent(display, &report);
-  }
-
-  return 1;
 }
